723840e7826a1f24f92cbbdfa82e2a7a8f78413c
Scott Triglia <scott.triglia@gmail.com>
2015-12-12 18:16:22 -0800

    Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='stockfighter',
-    version='0.1.0',
+    version='0.2.0',
     description="API wrapper for Stockfighter",
     long_description=readme + '\n\n' + history,
     author="Scott Triglia",
